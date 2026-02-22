78af062e6b65f4b9bbf9a9bbb50b12b7e1f8538f
sosman64 <sosman64@proton.me>
2026-02-22 04:37:58 +0200

    Create bash.nix

---
+++ configs/bash.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  home-manager.users.sosman64 =
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+
+    {
+      # --- GENERAL
+      programs.bash.enable = true;
+
+      # --- HOME
+      home.shellAliases.nrb = "sudo nixos-rebuild switch --no-reexer";
+      home.shellAliases.ngc = "sudo nix-collect-garbage --delete-older-than 1d && sudo /run/current-system/bin/switch-to-configuration boot";
+    }
+}
