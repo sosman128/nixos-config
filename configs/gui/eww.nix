6533eb43d585d81dd9b897ec6960dea87b5822c1
sosman64 <sosman64@proton.me>
2026-03-07 03:30:42 +0200

    edit eww.nix yucky wucky tucky ebewfehhuewdjdhj

--- configs/gui/eww.nix
+++ configs/gui/eww.nix
@@ -43,49 +43,90 @@
           };
         in
         ''
-        (defpoll workspace-number :interval "0.1s"
-                                  :initial "1"
-                                  `niri msg workspaces | grep "*" | cut -c 4`)
-        (defpoll volume :interval "0.1s"
-                        :initial "10%"
-                        `echo "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -c9-)"*100 | bc | awk '{printf("%d\n",$1 + 0.5)}' | cut -c -2`)
-        (defpoll hour :interval "0.1s"
-                      :initial "1"
-                      `date +%H`)
-        (defpoll minute :interval "0.1s"
-                        :initial "1"
-                        `date +%M`)
+        (defpoll workspace-number
+          :interval "0.1s"
+          :initial "1"
+          `niri msg workspaces | grep "*" | cut -c 4`)
+        (defpoll volume
+          :interval "0.1s"
+          :initial "10%"
+          `echo "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -c9-)"`)
+        (defpoll hour
+          :interval "0.1s"
+          :initial "1"
+          `date +%H`)
+        (defpoll minute
+          :interval "0.1s"
+          :initial "1"
+          `date +%M`)
         (defwidget workspace-indicator []
-                   (box :orientation "v"
-                        :space-evenly false
-                   (label :text workspace-number :valign "start" :css "label {background-color: ${workspaceIndicator.backgroundColor}; color:${workspaceIndicator.fontColor}; font-weight:${workspaceIndicator.fontWeight}; }")))
+          (box
+            :orientation "v"
+            :space-evenly false
+            (label
+             :text workspace-number
+             :valign "start"
+             :css "label
+               {background-color: ${workspaceIndicator.backgroundColor};
+                color:${workspaceIndicator.fontColor};
+                font-weight:${workspaceIndicator.fontWeight}; }")))
         (defwidget volume-indicator []
-                   (box :orientation "v"
-                        :space-evenly false
-                   (label :text volume :valign "start" :css "label {background-color: ${volumeIndicator.backgroundColor}; color:${volumeIndicator.fontColor}; font-weight:${volumeIndicator.fontWeight}; }")))
+          (box
+            :orientation "v"
+            :space-evenly false
+            (label
+             :text volume
+             :valign "start"
+             :css "label
+              {background-color: ${volumeIndicator.backgroundColor};
+              color:${volumeIndicator.fontColor};
+              font-weight:${volumeIndicator.fontWeight}; }")))
         (defwidget clock []
-                   (box :orientation "v"
-                        :space-evenly false
-                   (label :text hour :valign "start" :css "label { background-color:${clock.backgroundColor} ; color:${clock.fontColor} ; font-weight:${clock.fontWeight} ; }")
-                   (label :text minute :valign "start" :css "label {background-color: ${clock.backgroundColor}; color:${clock.fontColor} ; font-weight:${clock.fontWeight} ;}")))
-        (defwindow panel :monitor 0
-                         :geometry (geometry :x "0%"
-                                             :y "20px"
-                                             :width "${panel.width}"
-                                             :height "${panel.height}"
-                                             :anchor "${panel.anchor}")
-                         :stacking "fg"
-                         :exclusive true
-        (centerbox :orientation "v"
-                   :space-evenly false
-                   :vexpand true
-                   (clock :valign "start")
-                   (label :text "" :valign "center")
-                   (box :orientation "v"
-                        :space-evenly false
-                        :valign "end"
-                   (volume-indicator :valign "end")
-                   (workspace-indicator :valign "end"))))
+          (box
+            :orientation "v"
+            :space-evenly false
+            (label
+             :text hour
+             :valign "start"
+             :css "label
+              {background-color:${clock.backgroundColor} ;
+              color:${clock.fontColor} ;
+              font-weight:${clock.fontWeight} ; }")
+            (label
+              :text minute
+              :valign "start"
+              :css "label
+                {background-color: ${clock.backgroundColor};
+                color:${clock.fontColor} ;
+                font-weight:${clock.fontWeight} ;}")))
+        (defwindow panel
+          :monitor 0
+          :geometry
+            (geometry
+              :x "0%"
+              :y "20px"
+              :width "${panel.width}"
+              :height "${panel.height}"
+              :anchor "${panel.anchor}")
+          :stacking "fg"
+          :exclusive true
+        (centerbox
+          :orientation "v"
+          :space-evenly false
+          :vexpand true
+          (clock
+            :valign "start")
+          (label
+            :text ""
+            :valign "center")
+          (box
+            :orientation "v"
+            :space-evenly false
+            :valign "end"
+            (volume-indicator
+              :valign "end")
+            (workspace-indicator
+              :valign "end"))))
         '';
     };
 }
