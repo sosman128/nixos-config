6b737b01b89ec8d5ddd8932d52421bea605a8ed2
sosman64 <sosman64@proton.me>
2026-03-19 23:44:57 +0200

    formatting changes in starship.nix

--- configs/cli/starship.nix
+++ configs/cli/starship.nix
@@ -26,118 +26,80 @@
           "$schema" = 'https://starship.rs/config-schema.json'
           [battery]
           disabled = ${if batteryDisabled then "true" else "false"}
-
           [aws]
           symbol = " "
-
           [buf]
           symbol = " "
-
           [bun]
           symbol = " "
-
           [c]
           symbol = " "
-
           [cpp]
           symbol = " "
-
           [cmake]
           symbol = " "
-
           [conda]
           symbol = " "
-
           [crystal]
           symbol = " "
-
           [dart]
           symbol = " "
-
           [deno]
           symbol = " "
-
           [directory]
           read_only = " 󰌾"
-
           [docker_context]
           symbol = " "
-
           [elixir]
           symbol = " "
-
           [elm]
           symbol = " "
-
           [fennel]
           symbol = " "
-
           [fortran]
           symbol = " "
-
           [fossil_branch]
           symbol = " "
-
           [gcloud]
           symbol = " "
-
           [git_branch]
           symbol = " "
-
           [git_commit]
           tag_symbol = '  '
-
           [golang]
           symbol = " "
-
           [gradle]
           symbol = " "
-
           [guix_shell]
           symbol = " "
-
           [haskell]
           symbol = " "
-
           [haxe]
           symbol = " "
-
           [hg_branch]
           symbol = " "
-
           [hostname]
           ssh_symbol = " "
-
           [java]
           symbol = " "
-
           [julia]
           symbol = " "
-
           [kotlin]
           symbol = " "
-
           [lua]
           symbol = " "
-
           [memory_usage]
           symbol = "󰍛 "
-
           [meson]
           symbol = "󰔷 "
-
           [nim]
           symbol = "󰆥 "
-
           [nix_shell]
           symbol = " "
-
           [nodejs]
           symbol = " "
-
           [ocaml]
           symbol = " "
-
           [os.symbols]
           Alpaquita = " "
           Alpine = " "
@@ -188,46 +150,32 @@
           Void = " "
           Windows = "󰍲 "
           Zorin = " "
-
           [package]
           symbol = "󰏗 "
-
           [perl]
           symbol = " "
-
           [php]
           symbol = " "
-
           [pijul_channel]
           symbol = " "
-
           [pixi]
           symbol = "󰏗 "
-
           [python]
           symbol = " "
-
           [rlang]
           symbol = "󰟔 "
-
           [ruby]
           symbol = " "
-
           [rust]
           symbol = "󱘗 "
-
           [scala]
           symbol = " "
-
           [status]
           symbol = " "
-
           [swift]
           symbol = " "
-
           [xmake]
           symbol = " "
-
           [zig]
           symbol = " "
         '';
